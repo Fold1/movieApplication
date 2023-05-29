0fbbe118f8353b4c2b22a57b9f90c423f7de678e
Fold1 <f0ld1@mail.ru>
2023-05-30 03:45:15 +0700

    add modal

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const API_URL_POPULAR =
 	"https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
 const API_URL_SEARCH =
 	"https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+const API_URL_MOVIE_DETAILS =
+	"https://kinopoiskapiunofficial.tech/api/v2.2/films/";
 
 getMovies(API_URL_POPULAR);
 
@@ -14,7 +16,6 @@ async function getMovies(url) {
 		},
 	});
 	const respData = await resp.json();
-    console.log(respData)
 	showMovies(respData);
 }
 
@@ -31,7 +32,7 @@ function getClassByRate(vote) {
 }
 
 function convertToDecimal(vote) {
-	if (vote === "null" || vote === "undefined") {
+	if (vote === null || vote === "undefined" || vote === "null") {
 		return "0.0";
 	} else if (vote.endsWith("%")) {
 		let fixedVote = vote.slice(0, 2);
@@ -49,8 +50,8 @@ function convertToDecimal(vote) {
 function showMovies(data) {
 	const moviesEl = document.querySelector(".movies");
 
-    // Очистка пред. фильмов
-    document.querySelector(".movies").innerHTML = ""
+	// Очистка пред. фильмов
+	document.querySelector(".movies").innerHTML = "";
 
 	data.films.forEach((movie) => {
 		const movieEl = document.createElement("div");
@@ -72,19 +73,86 @@ function showMovies(data) {
 				)}">${convertToDecimal(movie.rating)}</div>
             </div>
         `;
+		movieEl.addEventListener("click", () => openModal(movie.filmId));
 		moviesEl.appendChild(movieEl);
 	});
 }
 
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 const search = document.querySelector(".header__search");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
+	e.preventDefault();
 
-    const apiSearchUrl = `${API_URL_SEARCH}${search.value}`
-    if(search.value) {
-        getMovies(apiSearchUrl)
-        search.value = ""
-    }
-})
+	const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
+	if (search.value) {
+		getMovies(apiSearchUrl);
+		search.value = "";
+	}
+});
+
+// Modal
+const modalEl = document.querySelector(".modal");
+
+async function openModal(id) {
+	const resp = await fetch(API_URL_MOVIE_DETAILS + id, {
+		headers: {
+			"Content-Type": "application/json",
+			"X-API-KEY": API_KEY,
+		},
+	});
+	const respData = await resp.json();
+
+	modalEl.classList.add("modal--show");
+	document.body.classList.add("stop-scrolling");
+
+	modalEl.innerHTML = `
+    <div class="modal__card">
+        <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
+        <h2>
+            <span class="modal__movie-title">${respData.nameRu} - </span>
+            <span class="modal__movie-release-year">${respData.year}</span>
+        </h2>
+        <ul class="modal__movie-info">
+            <div class="loader"></div>
+            <li class="modal__movie-genre">Жанр - ${respData.genres.map(
+				(el) => `<span> ${el.genre}</span>`
+			)}</li>
+            ${
+				respData.filmLength
+					? `<li class="modal__movie-runtime">Время - ${respData.filmLength} мин.</li>`
+					: ""
+			}
+            <li>Сайт: <a href="${
+				respData.webUrl
+			}" target="_blank" class="modal__movie-site">${
+		respData.webUrl
+	}</a></li>
+            <li class="modal__movie-overview">Описание: ${
+				respData.description
+			}</li>
+        </ul>
+        <button class="modal__button-close">Закрыть</button>
+    </div>
+`;
+	const btnClose = document.querySelector(".modal__button-close");
+	btnClose.addEventListener("click", () => closeModal());
+}
+
+function closeModal() {
+	modalEl.classList.remove("modal--show");
+    document.body.classList.remove("stop-scrolling");
+
+}
+
+window.addEventListener("click", (e) => {
+	if (e.target === modalEl) {
+		closeModal();
+	}
+});
+
+window.addEventListener("keydown", (e) => {
+	if (e.key === "Escape") {
+		closeModal();
+	}
+});
